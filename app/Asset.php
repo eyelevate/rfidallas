7671055b66f08a9cf57f68bc47816a41ba92e3f7
wondollaballa <onedough83@gmail.com>
2017-07-20 17:29:06 -0700

    asset model

--- app/Asset.php
+++ app/Asset.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Asset extends Model
 {
-    //
+    use SoftDeletes;
+
+    
 }
--- app/Device.php
+++ app/Device.php
@@ -9,6 +9,13 @@ class Device extends Model
 {
     use SoftDeletes;
 
+    public function assets()
+    {
+    	// companies may have many devices
+    	// one device may be applied to one company
+    	return $this->belongsToMany(Asset::class);
+    }
+
     public function deviceHistory()
     {
     	return $this->hasMany(DeviceHistory::class);
